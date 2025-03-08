8ab23f98513705b211b81b75a48e6123884b2531
mily <milydominik@proton.me>
2025-03-08 15:02:13 +0000

    Update src/pages/grades.py

--- src/pages/grades.py
+++ src/pages/grades.py
@@ -2,7 +2,7 @@ import flet as ft
 from i18n import _
 
 
-def GradesPage(page):  # Funkcja wymaga 'page' jako argument
+def GradesPage(page):
     modal = ft.AlertDialog(
         modal=True,
         title=ft.Text((_("Test modal"))),
@@ -16,10 +16,10 @@ def GradesPage(page):  # Funkcja wymaga 'page' jako argument
     header = ft.Container(
         content=ft.Column(
             [
-                ft.Text("Matematyka", size=15),
+                ft.Text("Example Subject", size=15),
                 ft.Row([
-                    ft.Text("4 "+(_("grades")), size=14),
-                    ft.Text((_("Average"))+": 1.00", size=14),
+                    ft.Text("2 "+(_("grades")), size=14),
+                    ft.Text((_("Average"))+": 5.00", size=14),
                 ])
             ],
             alignment=ft.MainAxisAlignment.CENTER,
@@ -35,20 +35,20 @@ def GradesPage(page):  # Funkcja wymaga 'page' jako argument
         content=ft.Column([
             ft.Row([
                 ft.Container(
-                    content=ft.Text("1", text_align=ft.TextAlign.CENTER),
+                    content=ft.Text("5", text_align=ft.TextAlign.CENTER),
                     bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
                     padding=10,
                     border_radius=5,
                     width=40,
                     margin=ft.margin.all(5),
-                    on_click=lambda e: page.open(modal)  # Użycie lambda, aby opóźnić wywołanie
+                    on_click=lambda e: page.open(modal)
                 ),
                 ft.Column(
                     [
-                        ft.Text("wiązania"),
+                        ft.Text("Example grade"),
                         ft.Row([
                             ft.Text("21.02.2025"),
-                            ft.Text("Waga: 1.0"),
+                            ft.Text((_("Weight"))+": 1.0"),
                         ])
                     ],
                     spacing=1
@@ -56,20 +56,20 @@ def GradesPage(page):  # Funkcja wymaga 'page' jako argument
             ]),
             ft.Row([
                 ft.Container(
-                    content=ft.Text("1", text_align=ft.TextAlign.CENTER),
+                    content=ft.Text("5", text_align=ft.TextAlign.CENTER),
                     bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
                     padding=10,
                     border_radius=5,
                     width=40,
                     margin=ft.margin.all(5),
-                    on_click=lambda e: page.open(modal)  # Użycie lambda, aby opóźnić wywołanie
+                    on_click=lambda e: page.open(modal)
                 ),
                 ft.Column(
                     [
-                        ft.Text("wiązania"),
+                        ft.Text("Example grade"),
                         ft.Row([
                             ft.Text("21.02.2025"),
-                            ft.Text("Waga: 1.0"),
+                            ft.Text((_("Weight"))+": 1.0"),
                         ])
                     ],
                     spacing=1
