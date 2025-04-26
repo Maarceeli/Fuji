26d9aa55ae619ab5f2d547fc5595890ecc53766d
Marceeli <marceli.f@outlook.com>
2025-04-26 09:32:50 +0200

    Add homework function to CoreInterface

--- src/sdk/src/interfaces/core/interface.py
+++ src/sdk/src/interfaces/core/interface.py
@@ -4,6 +4,7 @@ from sdk.src.models.grade import Grade
 from sdk.src.models.note import Note
 from sdk.src.models.student import Student
 from sdk.src.models.lesson import Lesson
+from sdk.src.models.homework import Homework
 
 
 class CoreInterface:
@@ -29,4 +30,7 @@ class CoreInterface:
         pass
     
     def get_timetable(from_: date, to: date) -> list[Lesson]:
+        pass
+
+    def get_homework(from_: date, to: date) -> list[Homework]:
         pass
